e558222d3f45ad2e8101043e5d0c938076eba6f1
Cyril <cyril.cordier@epitech.eu>
2020-08-17 15:56:20 +0200

    maj

--- tc95us/database/migrations/2014_10_12_000000_create_users_table.php
+++ tc95us/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('challengename');
+            $table->string('challengename')->nullable();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('email')->unique();
